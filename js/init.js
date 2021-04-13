bd6ee0fe2d73e1d359fddd462bc9206832d5eef0
Autentifica43OMC <81185264+Autentifica43OMC@users.noreply.github.com>
2021-04-13 14:59:53 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyAevprXVuwb2Rn5IyeUXGXxBDO3QsbtKd8",
-    authDomain: "autenfica43omc.firebaseapp.com",
-    projectId: "autenfica43omc",
-    storageBucket: "autenfica43omc.appspot.com",
-    messagingSenderId: "658882319842",
-    appId: "1:658882319842:web:ad0504b1a039f66ff0d5bb",
-    measurementId: "G-NCFV02MX05"
+  apiKey: "AIzaSyBn1rsG2Z089cD68TvE_Kep1ckgWGhUu30",
+    authDomain: "chatomc43-f61b2.firebaseapp.com",
+    projectId: "chatomc43-f61b2",
+    storageBucket: "chatomc43-f61b2.appspot.com",
+    messagingSenderId: "647616267070",
+    appId: "1:647616267070:web:8eca7f899f013351d9226f",
+    measurementId: "G-279N649YRF"
     });
